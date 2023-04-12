13e54ec0c252235bcd5343ba762778f76f7b7d24
Aleksandr <125996874+MrAlex-Neo@users.noreply.github.com>
2023-04-13 03:32:47 +0500

    jsBtn

--- js/script.js
+++ js/script.js
@@ -1,33 +1,36 @@
 let menuBtn = document.querySelector('.menu-btn');
 let menu = document.querySelector('.menu');
+let lang = document.querySelector('.lang');
+let uz = document.querySelector('.langUz')
+let usMan = document.querySelector('.userMenu')
+let mWind = document.querySelector('.userMenuWindow');
+let projectBtn = document.querySelectorAll('.btnClass')
+
+projectBtn.forEach( btn => {
+	btn.addEventListener('click', function () {
+		let projectBtn = event.target
+		document.querySelectorAll('.btnClass').forEach( btn => {
+			btn.classList.remove('secondary')
+		})
+		projectBtn.classList.add('secondary');
+	})
+  })
+
 
 menuBtn.addEventListener('click', function(){
 	menuBtn.classList.toggle('active');
 	menu.classList.toggle('active');
 })
 
-let lang = document.querySelector('.lang');
-let uz = document.querySelector('.langUz')
 
 lang.addEventListener('click', function(){
     uz.classList.toggle('flex');
 })
 
-
-// let activePrimary = document.querySelectorAll('.regPrime')
-// activePrimary.addEventListenerAll('click', function(){
-//   activePrimary.classList.add('activePrimary')
-//   activePrimary.classList.toggle('activePrimary')
-// })
-
-let usMan = document.querySelector('.userMenu')
-let mWind = document.querySelector('.userMenuWindow');
-
 usMan.addEventListener('click', function(){
 	mWind.classList.toggle('none');
     mWind.classList.toggle('flex')
 })
-
 
 
 function validate(evt) {
